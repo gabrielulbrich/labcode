b070273af5c3f5dbe435dee5c7c42b0c67263dfc
Gabriel Ulbrich <gabriel.ulbrich@mirumagency.com>
2019-03-01 17:02:49 -0300

    ajustes de nomes e diretorios 2

--- src/application/views/bootcamp/botao_dinamico.php
+++ src/application/views/bootcamp/botao_dinamico.php
@@ -6,11 +6,11 @@
   <title>Animais Fantásticos</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootcamp/botao-dinamico.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootcamp/botao-dinamico/botao-dinamico.css" />
 </head>
 <body>
 wda
-
+dw
   <script type="module" src="<?php echo base_url();?>assets/js/bootcamp/botao-dinamico/botao-dinamico.js"></script>
 </body>
 </html>
